f1cf77a109e6cdcbcdf3251091a596e5ad1c65de
𓆩𔘓𓆪 𝐕⊶𝐈⊶𝐊⊶𝐑⊶𝐀⊶𝐍⊶𝐓 𓆩𔘓𓆪 <166818922+VICKYCHOUDHARY1@users.noreply.github.com>
2025-01-08 18:47:34 +0530

    Update repo.py

--- NOBITA/plugins/bot/repo.py
+++ NOBITA/plugins/bot/repo.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 start_txt = """
 ✰ 𝗥ᴇᴘᴏ ᴛᴏ 𝗡ʜɪ 𝗠ɪʟᴇɢᴀ 𝗬ʜᴀ
 ✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
-✰ || @ll_NOBITA_BOT_DEVLOPER_ll ||
+✰ || @SYNTAX_WORLD||
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
 """
 
